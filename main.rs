a87bc03a2781b424e5dde811080b2b9048f6b057
rio-cl <147709250+rio-cl@users.noreply.github.com>
2023-10-27 08:57:47 +0100

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,20 +1,8 @@
 fn main() {
-    let sum = 5550 + 7310;
-    println!("The sum of 5550 and 7310 is:{}",sum );
+    let mut fees = 25_000;
+    println!("fees is {}",fees );
 
 
-    let difference:f32 = 95.50 - 4.30;
-    println!("The difference of 95.5 and 4.3 is: {}", difference );
-
-
-    let product:u32 = 4 * 30;
-    println!("The multiplication of 4 and 30 is: {}", product );
-
-
-    let quotient:f64 = 56.70 / 32.20;
-    println!("The division od 56.7 and 32.2 is: {}",quotient );
-
-
-    let remainder:f64 = 43.0 % 5.0;
-    println!("The remiander of 43 and 5 is: {}",remainder );
+    let fees = 35_000;
+    println!(" fees changed is {}",fees  );
 }
