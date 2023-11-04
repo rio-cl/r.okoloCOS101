2d8eeb07b3f61f8ed9743dd9ee2e6877270a9738
rio-cl <147709250+rio-cl@users.noreply.github.com>
2023-11-05 00:26:56 +0100

    week-4 project_! correction

--- week-4/project_1/src/main.rs
+++ week-4/project_1/src/main.rs
@@ -5,17 +5,20 @@ fn main()
     let mut input1 = String::new();
     let mut input2 = String::new();
 
-    println!("Enter distance of car:");
+    println!("\nEnter distance of car : ");
     io::stdin().read_line(&mut input1).expect("Not a valid string");
-    let a:f32 = input1.trim().parse().expect("Not a valid number ");
+    let mut d:f32 = input1.trim().parse().expect("Not a valid number ");
+   //changing to kilometer
+    d = d * 1.60934;
+    println!("The Distance in Kilometers is {} Km", d);
 
-    println!("Enter time spend during the trip");
+    println!("Enter time taken to cover distance :");
     io::stdin().read_line(&mut input2).expect("Not a valid string");
-    let b:f32 = input2.trim().parse().expect("Not a valid number");
+    let  t:f32 = input2.trim().parse().expect("Not a valid number");
 
-    let s:f32 = a / b ;
+    let s:f32 = d / t ;
     let  area = s;
     
 
-    println!(" The speed of the car is: {}km/h ", area);
+    println!("The speed of the car is: {} km/h \n ", area);
 }
