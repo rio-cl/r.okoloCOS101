3bea1e930647e18075039da28bb4a6379398e51a
rio-cl <147709250+rio-cl@users.noreply.github.com>
2023-11-04 15:56:03 +0100

    project 1

--- week-4/project_1/src/main.rs
+++ week-4/project_1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() 
+{
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+
+    println!("Enter distance of car:");
+    io::stdin().read_line(&mut input1).expect("Not a valid string");
+    let a:f32 = input1.trim().parse().expect("Not a valid number ");
+
+    println!("Enter time spend during the trip");
+    io::stdin().read_line(&mut input2).expect("Not a valid string");
+    let b:f32 = input2.trim().parse().expect("Not a valid number");
+
+    let s:f32 = a / b ;
+    let  area = s;
+    
+
+    println!(" The speed of the car is: {}km/h ", area);
 }
